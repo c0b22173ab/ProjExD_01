c20194416f51950234d6dc0330b313a8a9d5612b
c0b22173 <c0b22173ab@edu.teu.ac.jp>
2023-09-26 18:37:03 +0900

    ennsyuu

--- pygame_image.py
+++ pygame_image.py
@@ -9,6 +9,7 @@ def main():
     
     #画像サーフェイス
     bg_img = pg.image.load("ex01_2/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("ex01_2/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img2 = pg.transform.rotozoom(kk_img, 10, 1.0)
@@ -19,11 +20,14 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%1600
+        x = tmr
         screen.blit(bg_img, [-x,0])
-        screen.blit(bg_img, [1600-x, 0])
-      
-        screen.blit(kk_imgs[tmr%2],[300,200])
+        screen.blit(bg_img2, [1600-x, 0])
+        screen.blit(bg_img, [3200-x,0])
+        screen.blit(kk_imgs[(tmr//50)%2],[300,200])
+        if x==3200:
+            x = 0
+          
         pg.display.update()
         tmr += 1        
         clock.tick(100)
