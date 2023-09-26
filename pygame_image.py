a1667009ebafb6b0bed5ba962fd1cb7563ac7ddf
c0b22173 <c0b22173ab@edu.teu.ac.jp>
2023-09-26 16:40:26 +0900

    課題2

--- pygame_image.py
+++ pygame_image.py
@@ -6,13 +6,20 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    
+    #画像サーフェイス
+    bg_img = pg.image.load("fig/9.png")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
